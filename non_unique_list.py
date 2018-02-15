b9546e739ad66fa10c6594b067f6f012650e02ff
apalashkin <Alexey.palashkin@gmail.com>
2018-02-15 14:28:29 +0300

    new: finished non_unique_list task

---
+++ non_unique_list.py
@@ -0,0 +1,17 @@
+import collections
+
+
+def checkio(data):
+    for i in filter(lambda x: x[1] == 1, collections.Counter(data).items()):
+        data.pop(data.index(i[0]))
+
+    return data
+
+
+if __name__ == "__main__":
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
