ffa92e78b222f9bf26db4b1dd8c140f5eae51ce9
apalashkin <Alexey.palashkin@gmail.com>
2018-02-20 12:35:24 +0300

    new: solve second_index

---
+++ elementary/second_index.py
@@ -0,0 +1,18 @@
+def second_index(text: str, symbol: str):
+    """
+        returns the second index of a symbol in a given text
+    """
+    try:
+        return text.index(symbol, text.index(symbol) + 1)
+    except ValueError:
+        return
+
+
+if __name__ == '__main__':
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
